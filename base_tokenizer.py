ac8a870400670515ca3614edd17a4986b09e7f20
jepoll <jegpole@gmail.com>
2026-06-07 14:03:04 +0300

    get_stats to count stats in-place

--- base_tokenizer.py
+++ base_tokenizer.py
@@ -11,8 +11,8 @@ def merge(ids, pair, idx):
             i += 1
     return new_ids
 
-def get_stats(ids):
-    counts = {}
+def get_stats(ids, counts=None):
+    counts = {} if counts is None else counts
     for pair in zip(ids, ids[1:]):
         counts[pair] = counts.get(pair, 0) + 1
     return counts
@@ -21,6 +21,7 @@ class Tokenizer:
     def __init__(self):
         self.merges = {}
         self.vocab = {}
+        self.pattern = {}
 
     def train(self, txt, vocab_size, verbose=False):
         pass
